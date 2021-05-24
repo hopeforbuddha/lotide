64d5993b94a697425b8db83cbc03a0fb21d7301e
Jacob <jacob.fitzgerald@outlook.com>
2021-05-24 21:21:51 +0000

    made a better assert feature

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ' + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log('🛑🛑🛑 Assertion Failed: ' + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("BOO", 'BOO');
+assertEqual(9, 4009);
