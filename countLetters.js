b29e6932379278f4dc773d5fec57e92f4238d358
Jacob <jacob.fitzgerald@outlook.com>
2021-05-26 23:22:24 +0000

    created countletters

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let newObj = {};
+
+  for (const letters of string) {
+    if (newObj[letters]) {
+      newObj[letters] += 1;
+    } else {
+      newObj[letters] = 1;
+    }
+    console.log(newObj);
+  }
+  return newObj;
+};
+
+countLetters("lighthouse in the house");
