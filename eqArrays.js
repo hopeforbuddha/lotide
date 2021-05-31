0fb7b260b53ebfd1d5fb22a429dd8dcacb28d51b
Jacob <jacob.fitzgerald@outlook.com>
2021-05-31 17:33:11 +0000

    refractored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 let eqArrays = function(array1, array2) {
   let sameLength = array1.length === array2.length
   if (sameLength === true) {
@@ -20,15 +12,4 @@ let eqArrays = function(array1, array2) {
   }
 }
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
-
-/*
-for (let i = 0; i < result.length; i++) {
-   assertEqual(result[i], expectedAnswer[i])
-}
-*/
+module.exports = eqArrays;
